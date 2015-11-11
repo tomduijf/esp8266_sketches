a8a4135b3e4ac1ff1309387f8275a7527c650b73
Tom Duijf <tom.duijf@gmail.com>
2015-11-11 01:01:46 +0100

    mqtt rgb ledstrip project

---
+++ mqtt_light_rgb/README.md
@@ -0,0 +1,13 @@
+# MQTT RGB Light - led controller
+
+Inspired by
+ * http://chaozlabs.blogspot.de/2015/08/esp8266-in-wild-wifi-led-controller-hack.html
+
+## A WiFi RGB LED controller, using MQTT, controlled by HASS
+My parts?
+ * http://www.aliexpress.com/item/rgb-WiFi-controller-1-port-control-15-rgb-lights-communicate-with-Android-phone-to-dim-output/32301423622.html
+ * http://www.aliexpress.com/item/Black-PCB-LED-Strip-5050-DC12V-Black-PCB-Board-IP65-Waterproof-60LED-m-5m-300LED-RGB/2033629286.html
+or
+ * http://www.aliexpress.com/item/5050-Double-Color-Temperature-LED-Strip-One-Chip-with-Warm-White-and-Cold-White-DC12V-60LED/32298725565.html
+
+
---
+++ mqtt_light_rgb/sketch.ino
@@ -0,0 +1,170 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>  // https://github.com/Imroy/pubsubclient
+#include <WiFiClient.h>
+
+// Update these with values suitable for your network.
+IPAddress mqtt_server(10, 10, 10, 10);
+
+const char *ssid =  "ssid";    // cannot be longer than 32 characters!
+const char *pass =  "password";    //
+
+const char *mqtt_device = "/HASS/light_RGB_01";
+const char *mqtt_bri = *mqtt_device + "/Brightness";
+const char *mqtt_col = *mqtt_device + "/Color";
+const char *mqtt_sw1 = *mqtt_device + "/Switch1";
+const char *mqtt_sw2 = *mqtt_device + "/Switch2";
+
+
+// RGB FET
+#define redPIN    15
+#define greenPIN  13
+#define bluePIN   12
+
+// W FET - FIXME - No idea what these are for yet.
+#define w1PIN     14
+#define w2PIN     4
+
+// onbaord green LED D1
+#define LEDPIN    5
+// onbaord red LED D2
+#define LED2PIN   1
+
+// note 
+// TX GPIO2 @Serial1 (Serial ONE)
+// RX GPIO3 @Serial    
+
+
+// #####################
+// #define PWMRANGE 255
+// #define PWM_VALUE 31
+//int gamma_table[PWM_VALUE+1] = {
+//    0, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 10, 11, 13, 16, 19, 23,
+//    27, 32, 38, 45, 54, 64, 76, 91, 108, 128, 152, 181, 215, 255
+//};
+// #define PWM_VALUE 63
+//int gamma_table[PWM_VALUE+1] = {
+//    0, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9, 10,
+//    11, 12, 13, 15, 17, 19, 21, 23, 26, 29, 32, 36, 40, 44, 49, 55,
+//    61, 68, 76, 85, 94, 105, 117, 131, 146, 162, 181, 202, 225, 250,
+//    279, 311, 346, 386, 430, 479, 534, 595, 663, 739, 824, 918, 1023
+//};
+#define PWM_VALUE 63
+int gamma_table[PWM_VALUE+1] = {
+    0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9, 10,
+    11, 12, 13, 15, 17, 19, 21, 23, 26, 29, 32, 36, 40, 44, 49, 55,
+    61, 68, 76, 85, 94, 105, 117, 131, 146, 162, 181, 202, 225, 250,
+    279, 311, 346, 386, 430, 479, 534, 595, 663, 739, 824, 918, 1023
+};
+// #####################
+
+#define LEDoff digitalWrite(LEDPIN,HIGH)
+#define LEDon digitalWrite(LEDPIN,LOW)
+
+#define LED2off digitalWrite(LED2PIN,HIGH)
+#define LED2on digitalWrite(LED2PIN,LOW)
+
+WiFiClient wclient;
+PubSubClient client(wclient,mqtt_server);
+
+
+void callback(const MQTT::Publish& pub) {
+  Serial1.print(pub.topic());
+  Serial1.print(" => ");
+  Serial1.println(pub.payload_string());
+  
+  String payload = pub.payload_string();
+
+  if (String(pub.topic()) == mqtt_col) {
+    int c1 = payload.indexOf(';');
+    int c2 = payload.indexOf(';', c1+1);
+
+    int red = map(payload.toInt(), 0, 100, 0, PWM_VALUE);
+    red = constrain(red, 0, PWM_VALUE);
+    int green = map(payload.substring(c1+1,c2).toInt(), 0, 100, 0, PWM_VALUE);
+    green = constrain(green, 0, PWM_VALUE);
+    int blue = map(payload.substring(c2+1).toInt(), 0, 100, 0, PWM_VALUE);
+    blue = constrain(blue, 0, PWM_VALUE);
+    
+    Serial1.print("Received values:");
+    Serial1.print("Red: " + String(payload.toInt()) + ", Green: " + String(payload.substring(c1+1,c2).toInt()) + ", Blue: " + String(payload.substring(c2+1).toInt()));
+    Serial1.print("Target values:");
+    Serial1.print("Red: " + String(red) + ", Green: " + String(green) + ", Blue: " + String(blue));
+    Serial1.print("");
+  
+    red = gamma_table[red];
+    green = gamma_table[green];
+    blue = gamma_table[blue];
+
+    analogWrite(redPIN, red);
+    analogWrite(greenPIN, green);
+    analogWrite(bluePIN, blue);
+  }
+  else if (String(pub.topic()) == mqtt_sw1) {
+    int value = map(payload.toInt(),0,100,0,PWM_VALUE);
+    value = constrain(value,0,PWM_VALUE);
+    value = gamma_table[value];
+    analogWrite(w1PIN, value);
+    //Serial1.println(value);
+  }
+  else if (String(pub.topic()) == mqtt_sw2) {
+    int value = map(payload.toInt(),0,100,0,PWM_VALUE);
+    value = constrain(value,0,PWM_VALUE);
+    value = gamma_table[value];
+    analogWrite(w2PIN, value);
+    //Serial1.println(value);
+  }
+}
+
+
+void setup()
+{
+  pinMode(LEDPIN, OUTPUT);  
+  pinMode(LED2PIN, OUTPUT);  
+  
+  pinMode(redPIN, OUTPUT);
+  pinMode(greenPIN, OUTPUT);
+  pinMode(bluePIN, OUTPUT);
+  pinMode(w1PIN, OUTPUT);
+  pinMode(w2PIN, OUTPUT);    
+  
+  // Setup console
+  Serial1.begin(115200);
+  delay(10);
+  Serial1.println();
+  Serial1.println();
+
+  client.set_callback(callback);
+
+  WiFi.begin(ssid, pass);
+
+  LEDon;
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    LED2off;
+    delay(500);
+    Serial1.print(".");
+    LED2on;
+  }
+
+  Serial1.println("");
+
+  Serial1.println("WiFi connected");
+  Serial1.println("IP address: ");
+  Serial1.println(WiFi.localIP());
+
+  if (client.connect(mqtt_device)) {
+    client.subscribe(mqtt_col);
+    client.subscribe(mqtt_sw1);
+    client.subscribe(mqtt_sw2);
+    Serial1.println("MQTT connected");  
+  }
+
+  Serial1.println("");
+}
+
+
+void loop()
+{
+  client.loop();
+}
+
