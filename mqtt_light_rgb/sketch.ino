dff948dcc0c98e79227d5b9ecc0c95ff40345377
Tom Duijf <tom.duijf@gmail.com>
2015-11-15 23:38:36 +0100

    small update

--- mqtt_light_rgb/sketch.ino
+++ mqtt_light_rgb/sketch.ino
@@ -5,14 +5,14 @@
 // Update these with values suitable for your network.
 IPAddress mqtt_server(10, 10, 10, 10);
 
-const char *ssid =  "ssid";    // cannot be longer than 32 characters!
-const char *pass =  "password";    //
+const char *ssid =  "ssid";
+const char *pass =  "password";
 
-const char *mqtt_device = "/HASS/light_RGB_01";
-const char *mqtt_bri = *mqtt_device + "/Brightness";
-const char *mqtt_col = *mqtt_device + "/Color";
-const char *mqtt_sw1 = *mqtt_device + "/Switch1";
-const char *mqtt_sw2 = *mqtt_device + "/Switch2";
+const char *mqtt_device = "home/test/light_RGB_01";
+const char *mqtt_bri    = "home/test/light_RGB_01/Brightness";
+const char *mqtt_col    = "home/test/light_RGB_01/Color";
+const char *mqtt_sw1    = "home/test/light_RGB_01/Switch1";
+const char *mqtt_sw2    = "home/test/light_RGB_01/Switch2";
 
 
 // RGB FET
@@ -48,12 +48,24 @@ const char *mqtt_sw2 = *mqtt_device + "/Switch2";
 //    61, 68, 76, 85, 94, 105, 117, 131, 146, 162, 181, 202, 225, 250,
 //    279, 311, 346, 386, 430, 479, 534, 595, 663, 739, 824, 918, 1023
 //};
-#define PWM_VALUE 63
+#define PWM_VALUE 255
 int gamma_table[PWM_VALUE+1] = {
-    0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9, 10,
-    11, 12, 13, 15, 17, 19, 21, 23, 26, 29, 32, 36, 40, 44, 49, 55,
-    61, 68, 76, 85, 94, 105, 117, 131, 146, 162, 181, 202, 225, 250,
-    279, 311, 346, 386, 430, 479, 534, 595, 663, 739, 824, 918, 1023
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
 };
 // #####################
 
@@ -75,21 +87,21 @@ void callback(const MQTT::Publish& pub) {
   String payload = pub.payload_string();
 
   if (String(pub.topic()) == mqtt_col) {
-    int c1 = payload.indexOf(';');
-    int c2 = payload.indexOf(';', c1+1);
+    int c1 = payload.indexOf(',');
+    int c2 = payload.indexOf(',', c1+1);
 
-    int red = map(payload.toInt(), 0, 100, 0, PWM_VALUE);
+    int red = map(payload.toInt(), 0, 255, 0, PWM_VALUE);
     red = constrain(red, 0, PWM_VALUE);
-    int green = map(payload.substring(c1+1,c2).toInt(), 0, 100, 0, PWM_VALUE);
+    int green = map(payload.substring(c1+1,c2).toInt(), 0, 255, 0, PWM_VALUE);
     green = constrain(green, 0, PWM_VALUE);
-    int blue = map(payload.substring(c2+1).toInt(), 0, 100, 0, PWM_VALUE);
+    int blue = map(payload.substring(c2+1).toInt(), 0, 255, 0, PWM_VALUE);
     blue = constrain(blue, 0, PWM_VALUE);
     
     Serial1.print("Received values:");
-    Serial1.print("Red: " + String(payload.toInt()) + ", Green: " + String(payload.substring(c1+1,c2).toInt()) + ", Blue: " + String(payload.substring(c2+1).toInt()));
+    Serial1.println("Red: " + String(payload.toInt()) + ", Green: " + String(payload.substring(c1+1,c2).toInt()) + ", Blue: " + String(payload.substring(c2+1).toInt()));
     Serial1.print("Target values:");
-    Serial1.print("Red: " + String(red) + ", Green: " + String(green) + ", Blue: " + String(blue));
-    Serial1.print("");
+    Serial1.println("Red: " + String(red) + ", Green: " + String(green) + ", Blue: " + String(blue));
+    Serial1.println("");
   
     red = gamma_table[red];
     green = gamma_table[green];
@@ -100,14 +112,14 @@ void callback(const MQTT::Publish& pub) {
     analogWrite(bluePIN, blue);
   }
   else if (String(pub.topic()) == mqtt_sw1) {
-    int value = map(payload.toInt(),0,100,0,PWM_VALUE);
+    int value = map(payload.toInt(),0,255,0,PWM_VALUE);
     value = constrain(value,0,PWM_VALUE);
     value = gamma_table[value];
     analogWrite(w1PIN, value);
     //Serial1.println(value);
   }
   else if (String(pub.topic()) == mqtt_sw2) {
-    int value = map(payload.toInt(),0,100,0,PWM_VALUE);
+    int value = map(payload.toInt(),0,255,0,PWM_VALUE);
     value = constrain(value,0,PWM_VALUE);
     value = gamma_table[value];
     analogWrite(w2PIN, value);
@@ -156,7 +168,13 @@ void setup()
     client.subscribe(mqtt_col);
     client.subscribe(mqtt_sw1);
     client.subscribe(mqtt_sw2);
-    Serial1.println("MQTT connected");  
+    Serial1.println("MQTT connected:");  
+
+    Serial1.println(mqtt_device);
+    Serial1.println(mqtt_bri);
+    Serial1.println(mqtt_col);
+    Serial1.println(mqtt_sw1);
+    Serial1.println(mqtt_sw2);
   }
 
   Serial1.println("");
